4e5b5e2f05d484eec5ce67d885f0e083b9d659e8
Vishwanath <vishwanathiti828@gmail.com>
2021-12-09 09:58:27 +0530

    added usecase 2

--- empWageCalc.java
+++ empWageCalc.java
@@ -1,11 +1,20 @@
 public class empWageCalc{
+	static int isPresent = 1;
+	static int wagePerHr = 20;
+	static int fullDayHr = 8;
+
 	public static void main(String[] args){
-		int val = 1 ; //is present
-		if (val==random_num()){
+		if (isPresent==random_num()){
 			System.out.println("Employee is present");
+			System.out.println("Daily wage is " + daily_wage());
 		}else{
 			System.out.println("Employee is absent");
+			System.out.println("Daily wage is 0");
 		}
+	}
+
+	public static int daily_wage(){
+		return wagePerHr*fullDayHr;
 	}
 
 	public static int random_num(){
