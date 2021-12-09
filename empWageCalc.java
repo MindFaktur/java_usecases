4bf043b87125729abf0f7ebba6398cb53e59d33d
Vishwanath <vishwanathiti828@gmail.com>
2021-12-09 16:41:15 +0530

    added usecase 4

--- empWageCalc.java
+++ empWageCalc.java
@@ -1,19 +1,18 @@
 public class empWageCalc{
-	static int isFullTime = 2;
-	static int isPartTime = 1;
-	static int wagePerHr = 20;
-	static int fullDayHr = 8;
-	static int partDayHr = 4;
 
 	public static void main(String[] args){
-		if ( isFullTime == random_num() ){
-			System.out.println("Employee has worked full time");
-			System.out.println("Daily wage is " + daily_wage(wagePerHr,fullDayHr));
-		}else if ( isPartTime == random_num() ){
-			System.out.println("Employee has worked part time");
-			System.out.println("Daily wage is " + daily_wage(wagePerHr,partDayHr));
-		}else{
-			System.out.println("Employee was absent and daily wage is 0");
+		final int isFullTime = 2, isPartTime = 1, wagePerHr = 20, fullDayHr = 8, partDayHr = 4;
+
+		int rand_num = random_num();
+		switch ( rand_num ){
+		case isFullTime:	System.out.println("Employee has worked full time");
+								System.out.println("Daily wage is " + daily_wage(wagePerHr,fullDayHr));
+								break;
+		case isPartTime:  System.out.println("Employee has worked part time");
+								System.out.println("Daily wage is " + daily_wage(wagePerHr,partDayHr));
+								break;
+		default:				System.out.println("Employee was absent and daily wage is 0");
+
 		}
 	}
 
@@ -24,4 +23,5 @@ public class empWageCalc{
 	public static int random_num(){
 		return (int)(Math.random() * 10 % 3);
 	}
+
 }
